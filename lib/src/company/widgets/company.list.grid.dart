b7ed331ded3300b2a914d171b9d5488c2ad984bb
WithcenterDev9 <withcenter.dev9@gmail.com>
2025-11-21 15:13:40 +0800

    refactor: adjustments in company list grid

--- lib/src/company/widgets/company.list.grid.dart
+++ lib/src/company/widgets/company.list.grid.dart
@@ -43,7 +43,6 @@ class CompanyListGrid extends StatelessWidget {
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 crossAxisSpacing: 8,
-                mainAxisSpacing: 8, // Add vertical spacing between cards
                 childAspectRatio: 1.2,
               ),
               itemCount: companies.length,
