5837b70de5066e3e032da8f4f6f7aee85e8b2150
JaeHo Song <thruthesky@gmail.com>
2025-12-05 12:02:13 +0800

    fix: update function_list.sh to enable remote function retrieval

--- scripts/function_list.sh
+++ scripts/function_list.sh
@@ -1,2 +1,2 @@
-# curl "https://philgo.com/func.php?func=get_functions"
-curl "https://local.philgo.com:444/func.php?func=get_functions"
+curl "https://philgo.com/func.php?func=get_functions"
+# curl "https://local.philgo.com:444/func.php?func=get_functions"
