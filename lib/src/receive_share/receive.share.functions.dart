af0ec4efc5b892ef8875b5882108c7e8016a09ed
Noe <withcenter.dev4@gmail.com>
2025-12-16 13:38:27 +0800

    update to support ios message

--- lib/src/receive_share/receive.share.functions.dart
+++ lib/src/receive_share/receive.share.functions.dart
@@ -32,6 +32,9 @@ Future<void> sendReceiveShareToChat(
     if (promises.isNotEmpty) {
       await Future.wait(promises);
     }
+    if (data.isNotEmpty && data[0].message!.isNotEmpty) {
+      await sendMessage(roomId: roomId, text: data[0].message!);
+    }
   }
 }
 
