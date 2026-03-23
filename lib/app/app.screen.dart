1c2001111e2df08ec7dc2847c3d0653282b20ea7
WithcenterDev9 <withcenter.dev9@gmail.com>
2026-03-23 10:28:16 +0800

    fix: show login button in FAB menu when user is not logged in

--- lib/app/app.screen.dart
+++ lib/app/app.screen.dart
@@ -18,6 +18,7 @@ import 'package:philgo/post/post.model.dart';
 import 'package:philgo/post/post_category_bottom_sheet.dart';
 import 'package:philgo/post/view/post.view.screen.dart';
 import 'package:philgo/setting/setting.state.dart';
+import 'package:philgo/user/login/user.login.screen.dart';
 import 'package:philgo/user/user.service.dart';
 import 'package:philgo/user/widgets/login_required_dialog.dart';
 import 'package:provider/provider.dart';
@@ -161,6 +162,16 @@ class _AppScreenState extends State<AppScreen> {
 
   /// 탭별 팝업 메뉴 아이템 목록
   List<AppFabMenuItem> _getMenuItems(BuildContext context, int currentIndex) {
+    if (!UserService.isLoggedIn) {
+      return [
+        AppFabMenuItem(
+          icon: FontAwesomeIcons.lightRightToBracket,
+          label: '로그인'.tr(),
+          onTap: () => UserLoginScreen.push(context),
+        ),
+      ];
+    }
+
     switch (currentIndex) {
       case 0: // 홈
       case 4: // 메뉴
