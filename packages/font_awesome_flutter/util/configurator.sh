3f34c7ec300fd1dbc4abb213524a4de0d53004d8
WithcenterDev9 <withcenter.dev9@gmail.com>
2026-03-24 18:05:51 +0800

    ui changes

--- packages/font_awesome_flutter/util/configurator.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-pushd "$(dirname "$0")" > /dev/null || exit
-cd ..
-dart ./util/lib/main.dart "$@"
-popd > /dev/null || exit
--- v6/screens/post/widgets/comic_action_button.dart
+++
@@ -1,3 +0,0 @@
-// Comic 스타일 액션 버튼 위젯
-// philgo_api 패키지의 ComicActionButton을 re-export합니다.
-export 'package:philgo_api/philgo_api.dart' show ComicActionButton;
